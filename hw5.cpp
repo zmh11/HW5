742e432c6566623f0053282b24c75617eae3974e
Zachary Hickey <zmhickey@Zacharys-MBP.local>
2020-10-05 09:37:48 -0500

    done

--- hw5.cpp
+++ hw5.cpp
@@ -4,34 +4,54 @@
 #include "hw5.h"
 
 /***
- * 
+ * Swap (vector int a , int b)
+ *  temp =  vector[a];
+ *  vector[a] = vector[b]
+ *  vector[b] = temp 
  **/
 void Swap(std::vector<std::string> &vec, int a, int b){
   std::string temp = vec[a];
   vec[a] = vec[b];
   vec[b] = temp;
   }
+
+/***
+ * bubble sort (vector)
+ * for ( k = 0 k< vector_size -1 k++ )
+ *  for( i = k+1 k<vector_size i++)
+ *    if( vector[k].compare(vector[i]) > 0)
+ *      swap (vector, k, i)
+ ***/ 
 void BubbleSort(std::vector<std::string> &vec){
-  for( int k = 0; k < vec.size()-1; k++){
-    for( int i = k+1; i < vec.size(); i++)
+  for( int k = 0; k < vec.size()-1; k++ ){
+    for( int i = k+1; i < vec.size(); i++ )
     {
-      if( vec[k].compare(vec[i]) > 0) 
+      if( vec[k].compare(vec[i]) > 0 ) 
         Swap( vec, k, i);
     }
   }
 } 
 
 /***
+ * insertion sort( vector, int n) 
  * 
+ * int i, k, key
+ * for(i = 1 i<n i++){
+ *  key=vector[i]
+ *  k = i -1
+ *  while (j>=0 && vector[k] > k){
+ *    vector[i+1]= vector[k]
+ *    k = k-1}
+ *    vector[k+1] = key}
  **/
 void InsertionSort_helper(std::vector<int>& v, int size){
-  if(size <= 1)
+  if( size <= 1 )
     return;
   InsertionSort_helper(v, size-1);
   int end = v[size - 1];
   int move = size - 2;
 
-  while (move >= 0 && end < v[move]){
+  while ( move >= 0 && end < v[move] ){
     v[move + 1] = v[move];
     move--;
   }
@@ -42,10 +62,19 @@ void InsertionSort(std::vector<int>& v){
 }
 
 /***
- * 
+ * Merge (vector, int l, int m, int r)
+ *  create a sub vector of vector[1-m]
+ *  create a sub vector of vector[m+1-r]
+ *  
+ *  recombine sub arrays looking comparing first
+ *  value in each
+ *  place the smallest value of the two back into Vector
+ *  
+ *  place any remaning in subvectors at end of vector
+ *  
  **/
 void Merge(std::vector<std::string> &vec, int l, int m, int r){
-  int ls = m -l + 1;
+  int ls = m - l + 1;
   int rs = r - m;
 
   std::string tempLeft[ls];
@@ -61,8 +90,8 @@ void Merge(std::vector<std::string> &vec, int l, int m, int r){
   int left = 0;
   int place = l;
 
-  while( right < rs && left < ls){
-    if( tempLeft[left].compare(tempRight[right]) <= 0){
+  while( right < rs && left < ls ){
+    if( tempLeft[left].compare(tempRight[right] ) <= 0){
       vec[place] = tempLeft[left];
       left++;
     }
@@ -73,19 +102,27 @@ void Merge(std::vector<std::string> &vec, int l, int m, int r){
     place++;
     }
 
-  while( left < ls){
+  while( left < ls ){
     vec[place] = tempLeft[left];
     left++;
     place++;
     }
 
-    while( right < rs){
+    while( right < rs ){
     vec[place] = tempRight[right];
     right++;
     place++;
     }
   }
 
+/***
+ * MergeSort helper(vector, int l, int r)
+ * if ( l<r ){
+ *  MergeSort helper(vector,  l,  middle)
+ *  MergeSort helper(vector,  middle+1,  r)
+ *  Mearge (vector,  l,  middle, r)
+ * }
+ **/
 void MergeSort_helper(std::vector<std::string> &vec, int l, int r){
   if ( l<r ){
     int middle = l + (r-l)/2; 
@@ -94,6 +131,11 @@ void MergeSort_helper(std::vector<std::string> &vec, int l, int r){
     Merge(vec, l, middle, r);
   }
 }
+
+/***
+ * merge sort (vector)
+ *  MergeSort_helper(vector, 0, vector.size()-1)
+ **/
 void MergeSort(std::vector<std::string> &vec){
   MergeSort_helper(vec, 0, vec.size()-1);
 }
